64a707810402b55be90a9bdd204ac4eebed1ac3d
Olivier Nottebart <olivier.nottebart@gmail.com>
2021-11-03 15:07:05 +0100

    Exercise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello from src/index.js!");
+const API_KEY = 'YOUR_API_KEY';
+const API_URL = `https://api.giphy.com/v1/gifs/random?tag=cat&api_key=${API_KEY}&limit=1`;
+
+// Create a working infinite slider
